7f75fe5cbd8745b76d0069fc472274d008a8ce88
Tarique-B-DevOps <tariqueatwork@gmail.com>
2026-01-15 12:40:51 +0000

    added ecr module

---
+++ modules/ecr/variables.tf
@@ -0,0 +1,16 @@
+variable "repository_name" {
+  description = "Name of the ECR repository"
+  type        = string
+}
+
+variable "image_tag_mutability" {
+  description = "The tag mutability setting for the repository"
+  type        = string
+  default     = "MUTABLE"
+}
+
+variable "scan_on_push" {
+  description = "Indicates whether images are scanned after being pushed to the repository"
+  type        = bool
+  default     = false
+}
